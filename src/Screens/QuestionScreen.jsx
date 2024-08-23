c8eb82482b64a1f951a75aa2b120f56ff9073494
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-08-23 14:25:39 +0530

    project setup

---
+++ src/Screens/QuestionScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const QuestionScreen = () => {
+  return (
+    <View>
+      <Text>QuestionScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default QuestionScreen;
